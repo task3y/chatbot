8ab280eb09ba8abe9099aced7c56599606d50879
task3y <aphalswal1810@gmail.com>
2026-02-06 21:02:20 +0530

    Basic code structure for chatbot application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import ChatbotIcon from "./components/ChatbotIcon";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="container">
+      <div className="chatbot-popup">
+        <div className="chatbot-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">Chatbot</h2>
 
-export default App
+          </div>
+          <button className="material-symbols-rounded">
+keyboard_arrow_down
+</button>
+        </div>
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there! <br /> How can I help you?
+            </p>
+          </div>
+          <div className="message user-message">
+            <p className="message-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, voluptate.
+            </p>
+          </div>
+        </div>
+        <div className="chat-footer">
+           <form action="#" className="chat-form">
+            <input type="text" placeholder="Type a message..." className="message-input" required/>
+            <button className="material-symbols-rounded">arrow_upward
+</button>
+           </form>
+           
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
